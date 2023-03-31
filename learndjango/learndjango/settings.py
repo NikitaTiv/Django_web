64ec005b75952c4d9be246d895b169503887431c
Nikita Titov <titovn811@gmail.com>
2023-03-31 19:47:10 +0700

    Change settings

--- learndjango/learndjango/settings.py
+++ learndjango/learndjango/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m-&j+8x9yg@gc%-ec0js_b7l8lbxbl5eke+65013qzav0!xim6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
